e3c351714f2ce5f177be27c771ffcf94c9d569f8
Sheetalbajarangbalivishwakarma <svishwakarma69@yahoo.in>
2016-10-19 17:05:51 +0530

    Finish Readme

---
+++ transpose...1.py
@@ -0,0 +1,20 @@
+a = []
+m=int(input("enter no of rows in the matrix"))
+n=int(input("enter no of columns in the matrix"))  
+for i in range(0,m) :
+ for j in range(0,n) :
+    a.append([])
+    
+    a[i].append((input("enter element")))
+for k in a:
+ print(k)
+trans =[]
+print("tranpose matrix :")
+for i in range(0,m) :
+    trans.append([i])
+    for j in range(0,n) :
+        trans[i].append(a[j][i])
+for t in trans:
+ print(t)
+  
+ 
